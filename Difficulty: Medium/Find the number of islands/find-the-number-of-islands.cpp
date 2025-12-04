f65da6ae6263d54038a6310dc2a3ae99de18e9da
Vivek Dhakad <148355380+Vivekdhakad04@users.noreply.github.com>
2025-12-04 17:58:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -1,36 +1,59 @@
 class Solution {
   public:
-    
-    
-    void dfs(int i, int j, vector<vector<bool>>& visited,int row, int col,vector<vector<char>>& grid){
-        visited[i][j] = true;
-        int x[] = {1,-1,0,0,1,1,-1,-1};
-        int y[] = {0,0,1,-1,1,-1,1,-1};
+    bool isSafe(int i, int j, vector<vector<char>>& grid)
+    {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+        return false;
         
-        for(int k = 0; k < 8; k++){
-            int nx = i + x[k];
-            int ny = j + y[k];
-            
-            if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 'L' && !visited[nx][ny])
-            dfs(nx,ny,visited,row,col,grid);
-        }
+    return true;
     }
     
+    void bfs(int i, int j, vector<vector<char>>& grid,vector<vector<bool>>& visited)
+    {
+        queue<pair<int,int>> que;
+        que.push({i,j});
+        visited[i][j] = true;
+        
+        while(!que.empty())
+        {
+            pair<int,int> p = que.front();
+            que.pop();
+            i = p.first;
+            j = p.second;
+            for(int x = -1; x <= 1; x++)
+            {
+                for(int y = -1; y <=1; y++)
+                {
+                    if(x == 0 && y== 0)
+                    continue;
+                    
+                    if(isSafe(i+x,j+y,grid) && !visited[i+x][j+y] && grid[i+x][j+y] == 'L')
+                    {
+                        visited[i+x][j+y] = true;
+                        que.push({i+x,j+y});
+                    }
+                }
+            }
+        }
+    }
     int countIslands(vector<vector<char>>& grid) {
         // Code here
         int row = grid.size();
         int col = grid[0].size();
-        vector<vector<bool>> visited(row,vector<bool>(col, false));
+        int count = 0;
+        vector<vector<bool>> visited(row, vector<bool>(col ,false));
         
-        int island = 0;
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(grid[i][j] == 'L' && !visited[i][j]){
-                    island++;
-                    dfs(i,j,visited,row,col,grid);
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(!visited[i][j] && grid[i][j] == 'L')
+                {
+                    bfs(i,j,grid,visited);
+                    count++;
                 }
             }
         }
-    return island;
+        return count;
     }
 };
